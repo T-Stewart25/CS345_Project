3a8838d9a3aa80044569c5bde3c1d60951929a0a
Thomas Stewart <thomaslstewart1@gmail.com>
2024-04-09 09:13:17 -0600

    added GPU acceloration for NN

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from data import *
 from nearest_neighbor import *
 
 def nearest(X_train, y_train, X_test, y_test):
-    nn = nearest_neighbor()
+    nn = nearest_neighbor_torch()
     nn.fit(X_train, y_train)
+
     y_pred = np.array([nn.predict(X_test[i]) for i in range(len(X_test))])
+   
     return y_pred
     
 def accuracy(y_pred, y_test):
@@ -12,16 +14,19 @@ def accuracy(y_pred, y_test):
 
 
 if __name__ == '__main__':
-    X_train, y_train, X_test, y_test = preprocess_data('training.csv', 'testing.csv')
-    X_train_std, y_train_std, X_test_std, y_test_std = preprocess_data_with_standardization('training.csv', 'testing.csv')
-    X_train_norm, y_train_norm, X_test_norm, y_test_norm = preprocess_data_with_normalization('training.csv', 'testing.csv')
-    pred = nearest(X_train, y_train, X_test, y_test)
-    print(f'The accuracy of nearest neighbor {accuracy(pred, y_test):.3f}')
-    std_pred = nearest(X_train_std, y_train_std, X_test_std, y_test_std)
-    print(f'The accuracy of standardized nearest neighbor {accuracy(std_pred, y_test_std):.3f}')
-    norm_pred = nearest(X_train_norm, y_train_norm, X_test_norm, y_test_norm)
-    print(f'The accuracy of normalized nearest neighbor {accuracy(norm_pred, y_test_norm):.3f}')
-    
-    
 
+        X_train, y_train, X_test, y_test = preprocess_data('training.csv', 'testing.csv')
+        X_train_std, y_train_std, X_test_std, y_test_std = preprocess_data_with_standardization('training.csv', 'testing.csv')
+        X_train_norm, y_train_norm, X_test_norm, y_test_norm = preprocess_data_with_normalization('training.csv', 'testing.csv')
+        print("The accuracy of nearest neighbor, standardized nearest neighbor, and normalized nearest neighbor are as follows:")
+        pred = nearest(X_train, y_train, X_test, y_test)
+        print("Training and testing data loaded successfully for basic data")
+        print(f'The accuracy of nearest neighbor {accuracy(pred, y_test):.3f}')
+        std_pred = nearest(X_train_std, y_train_std, X_test_std, y_test_std)
+        print("Training and testing data loaded successfully for standarduzed data")
+        print(f'The accuracy of standardized nearest neighbor {accuracy(std_pred, y_test_std):.3f}')
+        norm_pred = nearest(X_train_norm, y_train_norm, X_test_norm, y_test_norm)
+        print("Training and testing data loaded successfully for normalized data")
+        print(f'The accuracy of normalized nearest neighbor {accuracy(norm_pred, y_test_norm):.3f}')
 
+        print("An exception occurred:", e)
--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -1,18 +1,40 @@
-from sklearn.neighbors import KNeighborsClassifier
+# Description: This file contains the implementation of the nearest neighbor classifier.
 import numpy as np
+import torch
 
 class nearest_neighbor:
     def __init__(self):
         pass
     def fit(self, X, y):
-        self.X = np.array(X)
-        self.y = np.array(y)
-    def predict(self, X_test) :
-        y_pred = np.array([])
+        """train a nearest neighbor classifier.  Nothing much to do!"""
+        self.X = X
+        self.y = y
+    def get_nearest(self, x):
+        """returns the index of the training example closest to x"""
+        distances = [distance(x, self.X[i]) 
+                     for i in range(len(self.X))]
+        return np.argmin(distances)
+    def predict(self, x) :
+        return self.y[self.get_nearest(x)]
 
-        for x in X_test:
-            predicted_class = self.y[np.argmin(np.linalg.norm(x-self.X, axis=1))]
-            y_pred = np.append(y_pred, predicted_class)
-        return y_pred
+
+#GPU ACCERATED  
+class nearest_neighbor_torch:
+    def __init__(self):
+        pass
     
+    def fit(self, X, y):
+        """Train a nearest neighbor classifier. Nothing much to do!"""
+        self.X = torch.tensor(X)  # Convert to PyTorch tensor
+        self.y = torch.tensor(y)  # Convert to PyTorch tensor
     
+    def get_nearest(self, x):
+        """Returns the index of the training example closest to x"""
+        distances = torch.stack([torch.norm(x - self.X[i]) 
+                                 for i in range(len(self.X))])
+        return torch.argmin(distances)
+    
+    def predict(self, x):
+        x_tensor = torch.tensor(x)  # Convert input to PyTorch tensor
+        nearest_index = self.get_nearest(x_tensor)
+        return self.y[nearest_index].item()
