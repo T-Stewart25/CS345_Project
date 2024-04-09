f50ec429cccbc5decf2efd560e3cf56a5e8c2f6e
Thomas Stewart <thomaslstewart1@gmail.com>
2024-04-09 07:58:37 -0600

    spacing

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from data import *
 
+
+
+
+
+
+
+
 if __name__ == '__main__':
     X_train_std, y_train_std, X_test_std, y_test_std = preprocess_data_with_standardization('training.csv', 'testing.csv')
     X_train_norm, y_train_norm, X_test_norm, y_test_norm = preprocess_data_with_normalization('training.csv', 'testing.csv')
-    print(X_train_std.iloc[0])
-    print("BREAK")
-    print(X_train_norm.iloc[0])
+    
     
 
 
